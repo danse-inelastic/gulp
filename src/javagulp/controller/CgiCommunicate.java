6354b7b42313c261313e8f0a0f0c62199fd8a0e6
J Brandon Keith <jbrkeith@gmail.com>
2009-02-24 12:40:10 +0000

    fixing the cgi stuff
    git-svn-id: svn://danse.us/MolDyn/javaGulpTrunk/Javagulp@245 a3a1b2a8-efeb-4e4f-91fb-0cb55f2ba858

--- src/javagulp/controller/CgiCommunicate.java
+++ src/javagulp/controller/CgiCommunicate.java
@@ -1,17 +1,13 @@
 package javagulp.controller;
 
-import java.io.BufferedReader;
 import java.io.IOException;
-import java.io.InputStreamReader;
 import java.io.OutputStreamWriter;
 import java.io.UnsupportedEncodingException;
 import java.net.MalformedURLException;
 import java.net.URL;
 import java.net.URLConnection;
 import java.net.URLEncoder;
-import java.util.Iterator;
 import java.util.Map;
-import java.util.Set;
 
 
 
