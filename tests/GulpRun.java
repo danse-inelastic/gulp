f043417a937f8d07b8c67b78c53dc23707ec9a3b
J Brandon Keith <jbrkeith@gmail.com>
2009-02-26 01:00:41 +0000

    more reorganization due to svn error problems...
    git-svn-id: svn://danse.us/MolDyn/javaGulpTrunk/Javagulp@255 a3a1b2a8-efeb-4e4f-91fb-0cb55f2ba858

---
+++ tests/GulpRun.java
@@ -0,0 +1,296 @@
+
+
+import java.awt.BorderLayout;
+import java.io.Serializable;
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.ResultSet;
+import java.sql.Statement;
+import java.util.HashMap;
+import java.util.Map;
+import java.util.Properties;
+
+import javagulp.model.Keywords;
+import javagulp.model.Material;
+import javagulp.view.Structures.Structure;
+
+//import org.postgresql.util.PSQLException
+
+import javax.swing.JPanel;
+import javax.swing.JScrollPane;
+import javax.swing.JTabbedPane;
+import javax.swing.event.ChangeEvent;
+import javax.swing.event.ChangeListener;
+
+public class GulpRun extends JPanel implements Serializable {
+
+	private static final long serialVersionUID = -4350272075095363083L;
+
+	private String[] tabNames = { "MolecularDynamics", "MonteCarlo",
+			"EnergeticsMatProp", "Optimization", "Constraints", "Fit",
+			"Phonons", "FreeEnergy", "TransitionState", "StructurePrediction", 
+			"Surface", "ExternalForce","Structures", "Potential",
+			"PotentialOptions", "ChargesElementsBonding", "Electrostatics",
+			"EwaldOptions", "Output", "Execution" };
+	
+	//private String[] bottomNames = {};
+
+	private JPanel[] top = new JPanel[tabNames.length];
+//	private JPanel[] bottom = new JPanel[bottomNames.length];
+
+	public JTabbedPane topPane = new JTabbedPane();
+	public JScrollPane topScroll = new JScrollPane(topPane);
+	private TopListener keyTop = new TopListener();
+
+	public Map<String,String> keyVals;
+//	public JTabbedPane bottomPane = new JTabbedPane();
+//	public JScrollPane bottomScroll = new JScrollPane(bottomPane);
+
+//	private JSplitPane splitPane = new JSplitPane(JSplitPane.VERTICAL_SPLIT,
+//			topScroll, bottomScroll);
+
+	private Keywords keywords = null;
+
+	public GulpRun(String[] simulationParams) {
+		super();
+		//setLayout(new CardLayout());
+		setLayout(new BorderLayout());
+
+//		splitPane.setDividerLocation((Back.frame.getHeight() - 135) / 2);
+//		splitPane.setDividerSize(4);
+//		splitPane.setResizeWeight(0.5);
+//		add(splitPane, BorderLayout.CENTER);
+		
+		add(topPane, BorderLayout.CENTER);
+
+		topPane.addChangeListener(keyTop);
+		topPane.add(null, "molecular dynamics");
+		topPane.add(null, "monte carlo");
+		topPane.add(null, "energetics and material properties");
+		topPane.add(null, "optimization");
+		topPane.add(null, "constraints");
+		topPane.add(null, "fit");
+		topPane.add(null, "phonons");
+		topPane.add(null, "free energy");
+		topPane.add(null, "transition state");
+		topPane.add(null, "structure prediction");
+		topPane.add(null, "surface");
+		topPane.add(null, "external force");
+
+		topPane.add(null, "structures");
+		topPane.add(null, "potentials");
+		topPane.add(null, "potential options");
+		topPane.add(null, "charges, elements and bonding");
+		topPane.add(null, "electrostatics");
+		topPane.add(null, "ewald options");
+
+		topPane.add(null, "output");
+		topPane.add(null, "execution");
+	}
+	
+	private void processArguments(String[] simulationParams) {
+		keyVals = new HashMap<String,String>();
+		if (simulationParams.length==0){
+			return;
+			//simulationParams=new String[]{"username=demo","ticket=5X","simulationId=1","matterId=9TAL9D"};
+		} else{
+		for(String param: simulationParams){
+			String[] keyVal = param.split("=");
+			keyVals.put(keyVal[0],keyVal[1]);
+		}
+		//retrieve matter and load it
+		Material mat = getMaterial(keyVals.get("materialId"));
+		getStructure().atomicCoordinates.getTableModel().importCoordinates(mat);
+		getStructure().unitCellAndSymmetry.unitCellPanel.threeDUnitCell.setVectors(mat);
+		}
+		//keep the rest of the parameters and pass them to the job submission post
+	}
+	
+	Material getMaterial(String id){
+		String query = "SELECT * FROM polycrystals WHERE id = '"+id+"' UNION "+
+		"SELECT * FROM singlecrystals WHERE id = '"+id+"' UNION "+
+		"SELECT * FROM disordered WHERE id = '"+id+"'";
+		//Array latticeArray = null;
+		double[] latticeVec = null;
+		//Array fractionalCoordinatesArray = null;
+		double[] fractionalCoordinatesVec = null;
+		String[] atomSymbols = null;
+		// get the material parameters from the db (eventually use ORM tool)
+		try {
+			Properties props = new Properties();
+//			props.setProperty("user","linjiao");
+//			props.setProperty("password","4OdACm#");
+//			String url = "jdbc:postgresql://localhost:54321/vnf";
+			props.setProperty("user","vnf");
+			props.setProperty("password","A4*gl8D");
+			String url = "jdbc:postgresql://vnf.caltech.edu:5432/vnf";
+			Class.forName("org.postgresql.Driver");
+			Connection con = DriverManager.getConnection(url,props);
+			Statement stmt = con.createStatement();
+			ResultSet rs = stmt.executeQuery(query);
+			rs.next();
+			///latticeArray = rs.getArray("cartesian_lattice");
+			latticeVec = (double[])rs.getArray("cartesian_lattice").getArray();
+			fractionalCoordinatesVec = (double[])rs.getArray("fractional_coordinates").getArray();
+			atomSymbols = (String[])rs.getArray("atom_symbols").getArray();
+			rs.close();
+			stmt.close();
+			con.close();
+		} catch (Exception ex) {
+			//System.out.println(ex.getMessage());
+			//System.out.println(ex.toString());
+			ex.printStackTrace();
+		}
+//		} catch (Exception e){
+//			
+//		}
+		Material mat = new Material();
+		mat.latticeVec = latticeVec;
+		mat.fractionalCoordinatesVec = fractionalCoordinatesVec;
+		mat.atomSymbols = atomSymbols;
+		return mat;
+	}
+
+
+	private class TopListener implements ChangeListener, Serializable {
+		private static final long serialVersionUID = -7619847591444570775L;
+		
+		public void stateChanged(ChangeEvent e) {
+			int index = topPane.getSelectedIndex();
+			topPane.setComponentAt(index, getTopPanel(index));
+		}
+	};
+
+	
+	private JPanel getTopPanel(int index) {
+		if (top[index] == null) {
+			String pkg = "javagulp.view.";
+			try {
+				Class c = Class.forName(pkg + tabNames[index]);
+				top[index] = (JPanel) c.newInstance();
+			} catch (ClassNotFoundException e) {
+				e.printStackTrace();
+			} catch (InstantiationException e) {
+				e.printStackTrace();
+			} catch (IllegalAccessException e) {
+				e.printStackTrace();
+			}
+		}
+		return top[index];
+	}
+	
+	public String getWD() {
+		return getExecution().txtWorkingDirectory.getText();
+	}
+	
+	public String getBinary() {
+		return getExecution().txtGulpBinary.getText();
+	}
+	
+	public Structure getStructure() {
+		return (Structure)getStructures().tabs.getSelectedComponent();
+	}
+	
+	public Keywords getKeywords() {
+		if (keywords == null)
+			keywords = new Keywords();
+		return keywords;
+	}
+	
+	//Top
+
+	public MolecularDynamics getMd() {
+		return (MolecularDynamics) getTopPanel(0);
+	}
+
+//	public MDRestartInit getMdRestartInit() {
+//		return (MDRestartInit) getTopPanel(1);
+//	}
+
+	public MonteCarlo getMonteCarlo() {
+		return (MonteCarlo) getTopPanel(1);
+	}
+
+	private EnergeticsMatProp getEnergeticsMatProp() {
+		return (EnergeticsMatProp) getTopPanel(2);
+	}
+
+	public Optimization getOptimization() {
+		return (Optimization) getTopPanel(3);
+	}
+	
+	public Constraints getConstraints() {
+		return (Constraints) getTopPanel(4);
+	}
+	public Fit getFit() {
+		return (Fit) getTopPanel(5);
+	}
+
+//	public XYZFit getXyzfit() {
+//		return (XYZFit) getTopPanel(7);
+//	}
+
+	public Phonons getPhonon() {
+		return (Phonons) getTopPanel(6);
+	}
+	
+	public FreeEnergy getFreeEnergy() {
+		return (FreeEnergy) getTopPanel(7);
+	}
+
+	public TransitionState getTransitionState() {
+		return (TransitionState) getTopPanel(8);
+	}
+	
+	public StructurePrediction getStructurePrediction() {
+		return (StructurePrediction) getTopPanel(9);
+	}
+
+//	public GeneticAlgorithm getGeneticAlgorithm() {
+//		return (GeneticAlgorithm) getTopPanel(10);
+//	}
+
+//	public Defect getDefect() {
+//		return (Defect) getTopPanel(13);
+//	}
+
+	public Surface getSurface() {
+		return (Surface) getTopPanel(10);
+	}
+
+	public ExternalForce getExternalForce() {
+		return (ExternalForce) getTopPanel(11);
+	}
+	
+	public Structures getStructures() {
+		return (Structures) getTopPanel(12);
+	}
+
+	public Potential getPotential() {
+		return (Potential) getTopPanel(13);
+	}
+
+	public PotentialOptions getPotentialOptions() {
+		return (PotentialOptions) getTopPanel(14);
+	}
+
+	public ChargesElementsBonding getChargesElementsBonding() {
+		return (ChargesElementsBonding) getTopPanel(15);
+	}
+
+	public Electrostatics getElectrostatics() {
+		return (Electrostatics) getTopPanel(16);
+	}
+
+	public EwaldOptions getEwaldOptions() {
+		return (EwaldOptions) getTopPanel(17);
+	}
+
+	public Output getOutput() {
+		return (Output) getTopPanel(18);
+	}
+
+	public Execution getExecution() {
+		return (Execution) getTopPanel(19);
+	}
+}
---
+++ tests/Potential.java
@@ -0,0 +1,207 @@
+
+
+import java.awt.BorderLayout;
+import java.awt.Dimension;
+import java.awt.event.ActionEvent;
+import java.io.BufferedReader;
+import java.io.File;
+import java.io.FileReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.io.Serializable;
+import java.net.MalformedURLException;
+import java.net.URL;
+
+import javagulp.controller.IncompleteOptionException;
+import javagulp.model.JCopy;
+import javagulp.model.SerialListener;
+import javagulp.view.potential.CreateLibrary;
+import javax.swing.JButton;
+
+import javax.swing.JList;
+
+import javax.swing.DefaultListModel;
+import javax.swing.JFileChooser;
+import javax.swing.JPanel;
+import javax.swing.JScrollPane;
+import javax.swing.JSplitPane;
+import javax.swing.JTabbedPane;
+import javax.swing.JTextPane;
+import javax.swing.ListSelectionModel;
+import javax.swing.event.ListSelectionEvent;
+import javax.swing.event.ListSelectionListener;
+
+public class Potential extends JPanel {
+
+	private static final long serialVersionUID = 4991943378742898078L;
+	//private String libraryPath = "src/javagulp/view/potentialLibraries";
+	private String libraryPath = "";
+	private JList libraryList;
+	private DefaultListModel potentialListModel = new DefaultListModel();
+	ListSelectionModel listSelectionModel;
+	private String librarySelected = "";
+	public CreateLibrary createLibrary = new CreateLibrary();
+	public JPanel useLibrary = new JPanel();
+	final JScrollPane scrollPane = new JScrollPane();
+	
+	private JTextPane libraryDisplay = new JTextPane();
+	public Potential() {
+		super();
+		setLayout(new BorderLayout());
+
+		final JTabbedPane tabbedPane = new JTabbedPane();
+		add(tabbedPane);
+
+		useLibrary.setLayout(new BorderLayout());
+		tabbedPane.addTab("use library", null, useLibrary, null);
+		tabbedPane.addTab("create library", null, createLibrary, null);
+
+		final JSplitPane splitPane = new JSplitPane();
+		useLibrary.add(splitPane);
+
+		//add(scrollPane, BorderLayout.NORTH);
+
+		//final JPanel panel_1 = new JPanel();
+		
+		
+		splitPane.setRightComponent(scrollPane);
+		scrollPane.setViewportView(libraryDisplay);
+
+		final JPanel panel = new JPanel();
+		panel.setLayout(new BorderLayout());
+		splitPane.setLeftComponent(panel);
+		panel.setPreferredSize(new Dimension(0, 40));
+
+		final JButton importButton = new JButton();
+		importButton.setText("import");
+		importButton.setEnabled(false);
+		panel.add(importButton, BorderLayout.SOUTH);
+		importButton.addActionListener(keyLibrary);
+		
+		libraryList = new JList();
+		File dir = new File(libraryPath);
+	    //File dir = new File(".");
+		URL imageURL = getClass().getResource("images/myImage.gif");
+
+	    
+	    String[] children = dir.list();
+	    for (int i=0; i<children.length; i++) {
+	    	// Get filename of file or directory
+	    	potentialListModel.addElement(children[i]);
+	    }
+	    libraryList.setModel(potentialListModel);
+	    listSelectionModel = libraryList.getSelectionModel();
+        listSelectionModel.addListSelectionListener(
+                new LibraryListener());
+		panel.add(libraryList);
+	}
+	
+	private class LibraryListener implements
+	ListSelectionListener, Serializable {
+	private static final long serialVersionUID = -2720144256318780471L;
+
+	public void valueChanged(ListSelectionEvent e) {
+		librarySelected = (String) libraryList.getSelectedValue();
+
+		//String libraryContents = getURLContentAsString(libURL);
+		String libraryContents = getFileContents(libraryPath+'/'+librarySelected);
+		libraryDisplay.setText(libraryContents);
+	}
+};
+
+public String getURLContentAsString(URL url) {
+	String content = "";
+    try {
+        // Create a URL for the desired page
+//        URL url = new URL(urlString);
+        // Read all the text returned by the server
+        BufferedReader in = new BufferedReader(new InputStreamReader(url.openStream()));
+        String str;
+        while ((str = in.readLine()) != null) {
+            // str is one line of text; readLine() strips the newline character(s)
+        	content+=str;
+        	content+=System.getProperty("line.separator");
+        }
+        in.close();
+    } catch (MalformedURLException e) {
+    } catch (IOException e) {
+    }
+	return content;
+}
+
+	public String getFileContents(String filePath) {
+    //...checks on aFile are elided
+	File aFile = new File(filePath);
+    StringBuilder contents = new StringBuilder();
+    
+    try {
+      //use buffering, reading one line at a time
+      //FileReader always assumes default encoding is OK!
+      BufferedReader input =  new BufferedReader(new FileReader(aFile));
+      try {
+        String line = null; //not declared within while loop
+        /*
+        * readLine is a bit quirky :
+        * it returns the content of a line MINUS the newline.
+        * it returns null only for the END of the stream.
+        * it returns an empty String if two newlines appear in a row.
+        */
+        while (( line = input.readLine()) != null){
+          contents.append(line);
+          contents.append(System.getProperty("line.separator"));
+        }
+      }
+      finally {
+        input.close();
+      }
+    }
+    catch (IOException ex){
+      ex.printStackTrace();
+    }
+    
+    return contents.toString();
+  }
+
+
+private LibraryListener listMouseListener = new LibraryListener();
+	
+	private SerialListener keyLibrary = new SerialListener() {
+		private static final long serialVersionUID = 8698926269816312994L;
+
+		@Override
+		public void actionPerformed(ActionEvent e) {
+				JFileChooser findLibrary = new JFileChooser();
+				findLibrary.setCurrentDirectory(new File(Back.getPanel().getWD()));
+				if (JFileChooser.APPROVE_OPTION == findLibrary.showOpenDialog(getParent())) {
+					final File newLocation = new File(Back.getPanel().getWD() + "/"
+									+ findLibrary.getSelectedFile().getName());
+					try {
+						new JCopy().copy(findLibrary.getSelectedFile(), newLocation);
+					} catch (final Exception e1) {
+						e1.printStackTrace();
+					}
+					librarySelected = removeDotSomething(newLocation.getName());
+			}
+		}
+	};
+
+	private String removeDotSomething(final String name) {
+		final String[] newName = name.split("\\.");
+		return newName[0];
+	}
+
+	public String writeLibrary() throws IncompleteOptionException {
+		String lines = "";
+		if (librarySelected!="")
+			lines = "library " + librarySelected;
+		if (Back.getPanel().getPotentialOptions().chkDoNotInclude.isSelected())
+			lines += " nodump";
+		if (lines!="")
+			lines += Back.newLine;
+		return lines;
+	}
+	
+	
+	
+
+}
