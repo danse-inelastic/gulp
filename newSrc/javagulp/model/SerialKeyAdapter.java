920ad83a34288461285014c53de4cf7a53ffc7ec
J Brandon Keith <jbrkeith@gmail.com>
2009-02-26 18:22:22 +0000

    using netbeans to do some awwweeesssoommee debugging...what a great tool!
    git-svn-id: svn://danse.us/MolDyn/javaGulpTrunk/Javagulp@263 a3a1b2a8-efeb-4e4f-91fb-0cb55f2ba858

--- newSrc/javagulp/model/SerialKeyAdapter.java
+++
@@ -1,8 +0,0 @@
-package javagulp.model;
-
-import java.awt.event.KeyAdapter;
-import java.io.Serializable;
-
-public class SerialKeyAdapter extends KeyAdapter implements Serializable {
-	private static final long serialVersionUID = -2961683652814010030L;
-}
--- newSrc/javagulp/model/SerialMouseAdapter.java
+++
@@ -1,8 +0,0 @@
-package javagulp.model;
-
-import java.awt.event.MouseAdapter;
-import java.io.Serializable;
-
-public class SerialMouseAdapter extends MouseAdapter implements Serializable {
-	private static final long serialVersionUID = 6367300558294519732L;
-}
