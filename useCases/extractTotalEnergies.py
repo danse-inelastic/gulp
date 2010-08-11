0dd2685cd4193e0262ff87ad753cb08851c847ae
J Brandon Keith <jbrkeith@gmail.com>
2010-08-11 21:38:04 +0000

    * added python parser for qe total energies
    git-svn-id: svn://danse.us/MolDyn/javaGulpTrunk/Javagulp@855 a3a1b2a8-efeb-4e4f-91fb-0cb55f2ba858

---
+++ useCases/extractTotalEnergies.py
@@ -0,0 +1,12 @@
+qeOutput = 'scf3.300K.out'
+import os
+os.system("grep '!    total energy' "+qeOutput+" > temp")
+f = file('temp')
+lines = f.readlines()
+fenergies = file('energies.txt','w')
+rydbergToEv = 1#13.605698066
+for line in lines:
+    parts = line.split()
+    fenergies.write(str(float(parts[4])*rydbergToEv)+'\n')
+os.system('rm temp') 
+    
